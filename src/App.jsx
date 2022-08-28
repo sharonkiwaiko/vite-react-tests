57f78ecabc289566c5fd2bbd273fe12a0a9e5bd2
sharon k <kiwaiko@gmail.com>
2022-08-28 05:44:52 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <List />
       <ImportExamples />
+      <UsersList />
     </div>
   );
 }
--- src/components/import_examples/ImportExamples.jsx
+++ src/components/import_examples/ImportExamples.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useId } from "react";
 import { var1, func1 } from "./ex1";
 import * as clb from "../../code/cllback";
 import usrs from "../../assets/users0";
 console.log("var1 : " + var1);
 console.log("func1 : " + func1());
 console.log("var1 : " + var1);
-
+/*const thusers = usrs.map((user) => {
+  const id = useId();
+  <div id={id}>${user.firstName}</div>;
+});
+*/
 const ImportExamples = () => {
-  <h1>ImportExamples</h1>;
-  console.log("clb : ");
-  clb.test();
-  console.log(usrs);
-  let singleUser = usrs.map((user) => {
-    //let's add the firstname and lastname together
-    let fullName = user.firstName + " " + user.lastName;
-    return console.log(fullName);
-  });
+  return (
+    <>
+      <p>
+        <h1>ImportExamples</h1>
+      </p>
+    </>
+  );
 };
 
 export default ImportExamples;
+/*<div>${thusers}</div>clb.test();console.log("clb : "); 
+    
+    console.log(usrs);
+let singleUser = usrs.map((user) => {
+    //let's add the firstname and lastname together
+    let fullName = user.firstName + " " + user.lastName;
+    return console.log(fullName);
+  }
+  console.log("singleUser : "+singleUser)); 
+  */
