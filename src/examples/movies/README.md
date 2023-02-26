b4ffe1e4e1761c90c3b3573ef1a7ec65c4bff730
sharon k <kiwaiko@gmail.com>
2023-02-26 05:55:16 +0000

    rev movies

---
+++ src/examples/movies/README.md
@@ -0,0 +1,1 @@
+redirect to examples/movies/App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Movies from "./examples/movies/App";
 import AppUserFinder from "./AppUserFinder";
 
 //import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/*<App />*/}
-    <AppUserFinder />
+    {/*<App />
+     <AppUserFinder />
+     */}
+    <Movies />
   </React.StrictMode>
 );
