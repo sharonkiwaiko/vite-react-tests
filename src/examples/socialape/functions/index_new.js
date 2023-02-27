6a32f723bfe19aa2b5f1e563dac4e9a5b6179e95
Sharon <kiwaiko@gmail.com>
2023-02-27 18:29:52 +0200

    Update index_new.js

--- src/examples/socialape/functions/index_new.js
+++ src/examples/socialape/functions/index_new.js
@@ -1,20 +1,16 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const app = require("express")();
+const {FBAuth}= require('./util/fbAuth')
 const {getAllScreams}= require('./handlers/screams')
 const {signup,login}= require('./handlers/users')
-const {FBAuth}= require('./util/fbAuth')
 
-
-const app = require("express")();
-const firebase = require("firebase");
-
-
-firebase.initializeApp(firebaseConfig);
-
-//scream routes
+//////////////////////////////////
+// ROUTES
+//////////////////////////////////
+//scream
 app.get("/screams", getAllScreams)
 app.post("/scream", FBAuth,postOneScream)  
-//users routes
+//users
 app.post("/signup", signup);
 app.post("/login", login);
 
