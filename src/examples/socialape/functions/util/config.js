3a35a0c1674061891a73284dd3788888e4337c0e
Sharon <kiwaiko@gmail.com>
2023-02-27 17:19:00 +0200

    Create config.js

---
+++ src/examples/socialape/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyB0kWXrKqviuaF61GJOP_UFFFO-yQKjE14",
+  authDomain: "socialape-78488.firebaseapp.com",
+  databaseURL:
+    "https://socialape-78488-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "socialape-78488",
+  storageBucket: "socialape-78488.appspot.com",
+  messagingSenderId: "472174416647",
+  appId: "1:472174416647:web:18b495eaf7f47b149a2ba2",
+};
