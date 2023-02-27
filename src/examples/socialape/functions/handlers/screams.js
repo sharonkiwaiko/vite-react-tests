a205b663c260f35ea89273fbebc5e4da49152411
Sharon <kiwaiko@gmail.com>
2023-02-27 17:07:27 +0200

    Update screams.js

--- src/examples/socialape/functions/handlers/screams.js
+++ src/examples/socialape/functions/handlers/screams.js
@@ -1,10 +1,6 @@
 const{db} = require('../util/admin')
-exports.getAllScreams = {
-  /*
-  admin
-    .firestore()
-    */
-  db
+exports.getAllScreams =(req,res)=> {
+   db
     .collection("screams")
     .orderBy("createdAt", "desc")
     .get()
@@ -25,3 +21,31 @@ exports.getAllScreams = {
       res.status(500).json({error:err.code})
     });
 }
+exports.postOneScreams = (req, res) => {
+  
+  //by the time we got here we got verifyied by the middleware FBAuth
+  if(req.body.body.trim()===''){
+  return res.status(400).json({body:'body must not be empty'})
+  }
+    
+  const newScream = {
+    body: req.body.body,
+    //userHandle: req.body.userHandle, 
+    //this was before middleware, now we get the user
+    userHandle: req.user.handle, 
+    
+    createdAt: new Date().toISOString(),
+    //createdAt: admin.firestore.TimeStamp.fromDate(new Date()),
+  };
+  /* admin.firestore() */
+  db
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfuly` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
