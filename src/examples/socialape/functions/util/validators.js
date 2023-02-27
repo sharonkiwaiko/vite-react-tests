5b9c246ff1929c5fa1bfdba0639c7747ac897827
Sharon <kiwaiko@gmail.com>
2023-02-27 17:55:29 +0200

    Update validators.js

--- src/examples/socialape/functions/util/validators.js
+++ src/examples/socialape/functions/util/validators.js
@@ -1,1 +1,32 @@
 
+const isEmail = (email) => {
+  const regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+ if(email.match(regex)) return true;
+   else return false;
+  
+}
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
+exports.validateSignupData = (data) => {
+let errors = {};
+  if(isEmpty(data.email)){
+  errors.email = 'Email must not be empty'
+  } else if(!isEmail(data.email)){
+   errors.email = 'Email must be a valid email address'
+  }
+  
+  if(isEmpty(data.password)) errors.password =  'password must not be empty'
+  if(data.password != data.confirmPassword errors.confirmPassword =  'passwords must match'
+  if(isEmpty(data.handle)) errors.handle =  'must not be empty'
+  
+  if(Object.keys(errors).length>0) return res.status(400).json(errors);
+  // miss a key will trigger unexpected error we dont cover
+
+return{
+errors,
+  valid: Object.keys(errors).length === 0 ? true : false
+}
+}
