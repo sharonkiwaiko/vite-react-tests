a857be59ce3569e8406cf1bd72feca6eb6b3dc41
Sharon <kiwaiko@gmail.com>
2023-02-27 16:29:08 +0200

    Update screams.js
    added getAllScreams

--- src/examples/socialape/functions/handlers/screams.js
+++ src/examples/socialape/functions/handlers/screams.js
@@ -1,1 +1,24 @@
-//only screams
+const{db} = require('../util/admin')
+exports.getAllScreams = {
+  /*
+  admin
+    .firestore()
+    */
+  db
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+}
