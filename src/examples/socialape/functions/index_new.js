b4bd94ccaa44312b98cb643aee108bf430b50e25
Sharon <kiwaiko@gmail.com>
2023-02-27 17:12:15 +0200

    Update index_new.js

--- src/examples/socialape/functions/index_new.js
+++ src/examples/socialape/functions/index_new.js
@@ -22,7 +22,7 @@ firebase.initializeApp(firebaseConfig);
 
 //scream routes
 app.get("/screams", getAllScreams)
-  
+app.post("/scream", FBAuth,postOneScream)  
 
 
 
@@ -58,8 +58,7 @@ if(req.headers.authorization && req.headers.authorization.startsWith(Bearer '){
     return res.status(403).json(err);
       })
  /////////////////////////////////////////////////////////////////   
-//Post one scream postOneScream
-app.post("/scream", FBAuth,postOneScream)
+
 
 //exports.api = functions.https.onRequest(app);
 
@@ -77,65 +76,7 @@ const isEmpty = (string) => {
 }
 
 //signup route
-app.post("/signup", (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle,
-  };
-  let errors = {};
-  if(isEmpty(newUser.email)){
-  errors.email = 'Email must not be empty'
-  } else if(!isEmail(newUser.email)){
-   errors.email = 'Email must be a valid email address'
-  }
-  
-  if(isEmpty(newUser.password)) errors.password =  'password must not be empty'
-  if(newUser.password != newUser.confirmPassword errors.confirmPassword =  'passwords must match'
-  if(isEmpty(newUser.handle)) errors.handle =  'must not be empty'
-  
-  if(Object.keys(errors).length>0) return res.status(400).json(errors);
-  // miss a key will trigger unexpected error we dont cover
-  //TODO validate data
-  ////////////////////////////////////////
-  let token,userId;
-  db.doc(`/users/`${newUser.handle}`).get()
-  .then(doc => {
-  if(doc.exists){
-  return res.status(400).json({handle:'this handle is already taken'})
-  } else {
-  return firebase
-  /////
-    .auth()
-    .createUserWithEmaiAndPassword(newUser.email, newUser.password);
-    }
-    })
-    .then(data => {
-    userId = data.user.uid;
-    return data.user.getIdToken()
-    })
-    .then(idToken => {
-    token=idToken;
-    //  return res.status(201).json({token});
-    const userCredantials = {
-    handle:newUser.handle,
-    email:newUser.email,
-    createdAt: new Date().toISOString(),
-    userId
-    }
-    return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-     })
-     .then((data)=>{
-    return res.status(201).json({token});
-    })
-      .catch((err) => {
-      console.error(err);
-      if(err.code === 'auth/email-already-in-use')
-       return res.status(400).json({ email: 'Email already in use' });
-       } else {
-      return res.status(500).json({ error: err.code });
-    });
+app.post("/signup", signup);
     
     //signup login
     app.post("/login", (req, res) => {
