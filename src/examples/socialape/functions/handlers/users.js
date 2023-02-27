69761e9c655817c2c04fd20e2fdd82f6a9f6b1cf
Sharon <kiwaiko@gmail.com>
2023-02-27 18:16:58 +0200

    Update users.js

--- src/examples/socialape/functions/handlers/users.js
+++ src/examples/socialape/functions/handlers/users.js
@@ -77,14 +77,11 @@ exports.login =(req, res) => {
         }
         
         
-         const {valid, errors} = validateLoginData(user)
-  if(!valid) return res.status(400).json(errors)
+     const {valid, errors} = validateLoginData(user)
+    if(!valid) return res.status(400).json(errors)
   
   
-    let errors = {};
-    if(isEmpty(user.email)) errors.email =  'must not be empty';
-    if(isEmpty(user.password)) errors.password =  'must not be empty';
-    if(Object.keys(errors).length>0) return res.status(400).json(errors);
+    
     firebase.auth().signInWithEmailAndPassword(user.email,user.password)
     then(data =>{
     return data.user.getIdToken()
